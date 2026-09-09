6ca6d90eaa4f5d4f0bb51ce6fec7ea7d11ce5d2a
LaraSurana27 <232536721+LaraSurana27@users.noreply.github.com>
2026-09-09 20:52:44 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,31 +1,14 @@
-#define ll long long
 class Solution {
 public:
     long long countCommas(long long n) {
-        ll result = 0;
-        ll start = 1000;
-        // ll lower = 1000;
-        // ll comma = 1;
-
-        // while(lower <= n)
-        // {
-        //     ll upper = lower * 1000 - 1;
-        //     if(upper > n) 
-        //     {
-        //         upper = n;
-        //     }
-        //     ll count = upper - lower + 1;
-        //     result += (count * comma);
-
-        //     lower *= 1000;
-        //     comma++;
-        // }
-
-        while(start <= n)
-        {
-            result += (n - start + 1);
-            start *= 1000;
-        }
-        return result;
+        long long totalCommas = 0;
+        
+        if (n >= 1000LL)             totalCommas += n - 1000LL + 1;
+        if (n >= 1000000LL)          totalCommas += n - 1000000LL + 1;
+        if (n >= 1000000000LL)       totalCommas += n - 1000000000LL + 1;
+        if (n >= 1000000000000LL)    totalCommas += n - 1000000000000LL + 1;
+        if (n >= 1000000000000000LL) totalCommas += n - 1000000000000000 + 1;
+     
+        return totalCommas;
     }
 };
