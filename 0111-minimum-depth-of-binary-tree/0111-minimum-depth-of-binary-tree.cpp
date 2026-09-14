efff8e8419f6e2dd4660e6c73c7a5290f9afdeb0
LaraSurana27 <232536721+LaraSurana27@users.noreply.github.com>
2026-09-14 17:20:21 +0530

    Time: 0 ms (100%), Space: 147 MB (5.41%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,6 +12,28 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
+
+        /*
+        Agar ek node ka sirf ek hi child hai (dusra null), to us node ko leaf nahi maan sakte — warna wrong answer aayega.
+        Example 2 dekho:
+    2
+     \
+      3
+       \
+        4
+         \
+          5
+           \
+            6
+
+        Yaha 2 ka left child null hai, lekin 2 leaf nahi hai kyunki right child hai. Isliye seedha min(left, right) nahi le sakte — agar ek side null hai to us side ki depth ignore karni hai.
+
+        Logic:
+Agar root == null → return 0
+Agar left == null → sirf right subtree explore karo: 1 + minDepth(right)
+Agar right == null → sirf left subtree explore karo: 1 + minDepth(left)
+Agar dono hain → 1 + min(minDepth(left), minDepth(right))
+        */
      if(root == NULL) return 0;
 
      if(root -> left == NULL) return 1 + minDepth(root -> right);
