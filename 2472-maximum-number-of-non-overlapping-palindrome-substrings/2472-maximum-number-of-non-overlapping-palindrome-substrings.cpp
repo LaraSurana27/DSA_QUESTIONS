6f5b9deabb376cdab425ec1c788fa6743ce906b7
LaraSurana27 <232536721+LaraSurana27@users.noreply.github.com>
2026-09-15 17:09:26 +0530

    Time: 0 ms (100%), Space: 9.5 MB (79.06%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int n;
@@ -50,5 +51,37 @@ public:
         if(k == 1) return n;
         vector<vector<int>> t(n+1, vector<int>(n+1, -1));
         return solve(s, k , 0, k-1, t);
+    }
+};
+
+*/
+
+// grredy+dp
+
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        auto expand = [&](int l, int r) {
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 >= k) {
+                    // smallest valid palindrome at this center found
+                    dp[r + 1] = max(dp[r + 1], dp[l] + 1);
+                    break; // ruk jao, bada palindrome nahi chahiye
+                }
+                l--;
+                r++;
+            }
+        };
+
+        for (int i = 0; i < n; i++) {
+            expand(i, i);       // odd length center
+            expand(i, i + 1);   // even length center
+            dp[i+1] = max(dp[i+1], dp[i]); // carry forward
+        }
+        
+        return dp[n];
     }
 };
